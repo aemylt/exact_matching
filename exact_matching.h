8a2b672631fea070c4b608db08156ffc928dc4f0
Dominic Moylett <dm1905@bristol.ac.uk>
2014-08-08 12:57:53 +0100

    Exact matching working with part of the pattern.

--- exact_matching.h
+++ exact_matching.h
@@ -32,7 +32,7 @@ int fingerprint_match_allcrosses(char* T, int n, char* P, int m, int alpha, int*
     while ((1 << lm) <= m) lm++;
     lm++;
     fingerprinter printer = fingerprinter_build(n, alpha);
-    fingerprint tmp = init_fingerprint(), T_f = init_fingerprint();
+    fingerprint tmp = init_fingerprint(), T_f = init_fingerprint(), T_prev = init_fingerprint(), T_cur = init_fingerprint();
     pattern_row *P_i = malloc(lm * sizeof(pattern_row));
     P_i[0].P = init_fingerprint();
     set_fingerprint(printer, P, 1, P_i[0].P);
@@ -46,8 +46,7 @@ int fingerprint_match_allcrosses(char* T, int n, char* P, int m, int alpha, int*
     for (i = 1; i < lm - 1; i++) {
         P_i[i].P = init_fingerprint();
         j = 1 << (i - 1);
-        set_fingerprint(printer, &P[j], j, tmp);
-        fingerprint_concat(printer->p, P_i[i - 1].P, tmp, P_i[i].P);
+        set_fingerprint(printer, &P[j], j, P_i[i].P);
         P_i[i].row_size = j << 1;
         P_i[i].end = 0;
         P_i[i].VOs = malloc(P_i[i].row_size * sizeof(viable_occurance));
@@ -58,38 +57,39 @@ int fingerprint_match_allcrosses(char* T, int n, char* P, int m, int alpha, int*
     }
     P_i[lm - 1].P = init_fingerprint();
     j = 1 << (lm - 2);
-    set_fingerprint(printer, &P[j], m - j, tmp);
-    fingerprint_concat(printer->p, P_i[lm - 2].P, tmp, P_i[lm - 1].P);
+    set_fingerprint(printer, &P[j], m - j, P_i[lm - 1].P);
     P_i[lm - 1].row_size = 0;
 
     for (i = 0; i < n; i++) {
+        set_fingerprint(printer, &T[i], 1, T_cur);
+        fingerprint_concat(printer->p, T_prev, T_cur, tmp);
+
         j = lm - 2;
         if ((P_i[j].end > 0) && (i - P_i[j].VOs[0].location == m - P_i[j].row_size)) {
-            set_fingerprint(printer, &T[i - m + P_i[j].row_size + 1], m - P_i[j].row_size, tmp);
-            fingerprint_concat(printer->p, P_i[j].VOs[0].T_f, tmp, T_f);
+            fingerprint_suffix(printer->p, tmp, P_i[j].VOs[0].T_f, T_f);
 
-            if (fingerprint_equals (P_i[j + 1].P, T_f)) results[matches++] = i + 1;
+            if (fingerprint_equals(P_i[j + 1].P, T_f)) results[matches++] = i + 1;
             shift_row(&P_i[j]);
         }
 
         for (j = lm - 3; j >= 0; j--) {
             if ((P_i[j].end > 0) && (i - P_i[j].VOs[0].location == P_i[j].row_size)) {
-                set_fingerprint(printer, &T[i - P_i[j].row_size + 1], P_i[j].row_size, tmp);
-                fingerprint_concat(printer->p, P_i[j].VOs[0].T_f, tmp, T_f);
-                if (fingerprint_equals (P_i[j + 1].P, T_f)) {
-                    fingerprint_assign(T_f, P_i[j + 1].VOs[P_i[j + 1].end].T_f);
+                fingerprint_suffix(printer->p, tmp, P_i[j].VOs[0].T_f, T_f);
+
+                if (fingerprint_equals(P_i[j + 1].P, T_f)) {
+                    fingerprint_assign(tmp, P_i[j + 1].VOs[P_i[j + 1].end].T_f);
                     P_i[j + 1].VOs[P_i[j + 1].end].location = i;
                     P_i[j + 1].end++;
                 }
                 shift_row(&P_i[j]);
             }
         }
-        set_fingerprint(printer, &T[i], 1, T_f);
-        if (fingerprint_equals (P_i[0].P, T_f)) {
-            fingerprint_assign(T_f, P_i[0].VOs[P_i[0].end].T_f);
+        if (fingerprint_equals (P_i[0].P, T_cur)) {
+            fingerprint_assign(tmp, P_i[0].VOs[P_i[0].end].T_f);
             P_i[0].VOs[P_i[0].end].location = i;
             P_i[0].end++;
         }
+        fingerprint_assign(tmp, T_prev);
     }
     return matches;
 }
